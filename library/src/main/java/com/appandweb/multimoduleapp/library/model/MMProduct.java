c4728cca55ddac93ca783b879dc9c4c60c750539
Olmo Gallegos <olmo@appandweb.es>
2019-06-11 13:12:04 +0200

    Add MMProduct entity

---
+++ library/src/main/java/com/appandweb/multimoduleapp/library/model/MMProduct.java
@@ -0,0 +1,40 @@
+package com.appandweb.multimoduleapp.library.model;
+
+public class MMProduct {
+    String uid;
+    String name;
+    String thumbnail;
+    float price;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
